b1407fe9d554d59d404776751e6ca55b542c7666
dimarque <dimarque@c1r17s15.42lisboa.com>
2022-07-04 11:35:05 +0100

    C-00 	new file:   C-00/ex00/ft_putchar.c 	new file:   C-00/ex00/main.c 	new file:   C-00/ex01/ft_print_alphabet.c 	new file:   C-00/ex01/main.c 	new file:   C-00/ex02/ft_print_reverse_alphabet.c 	new file:   C-00/ex02/main.c 	new file:   C-00/ex03/ft_print_numbers.c 	new file:   C-00/ex03/main.c 	new file:   C-00/ex04/ft_is_negative.c 	new file:   C-00/ex04/main.c 	new file:   C-00/ex05/ft_print_comb.c 	new file:   C-00/ex05/main.c 	new file:   C-00/ex06/ft_print_comb2.c 	new file:   C-00/ex06/main.c 	new file:   C-00/ex07/ft_putnbr.c 	new file:   C-00/ex07/main.c

---
+++ C-00/ex06/ft_print_comb2.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dimarque <dimarque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 09:55:37 by dimarque          #+#    #+#             */
+/*   Updated: 2022/07/04 10:34:32 by dimarque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void  ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void ft_print_comb2(void)
+{
+  int i;
+  int j;
+  i = -1 ;
+  while (++i <= 98)
+  {
+    j = i;
+    while (++j <= 99)
+    {
+      ft_putchar((char)(i/10) + '0');
+      ft_putchar((char)(i%10) + '0');
+      ft_putchar(' ');
+      ft_putchar((char)(j/10) + '0');
+      ft_putchar((char)(j%10) + '0');
+      
+      
+      if ( (i != 98))
+      {
+        ft_putchar(',');
+        ft_putchar(' ');
+      }
+      
+    }
+  }
+}
