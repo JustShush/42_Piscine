081a0d116231fd7edb8bc0c63dab06b58a416dc2
dimarque <dimarque@c1r17s15.42lisboa.com>
2022-07-13 15:44:59 +0100

    NO MORE MAIN!!!! 	modified:   C/C-03/ex00/ft_strcmp.c 	modified:   C/C-03/ex01/ft_strncmp.c 	modified:   C/C-03/ex02/ft_strcat.c 	modified:   C/C-03/ex03/ft_strncat.c

--- C/C-03/ex01/ft_strncmp.c
+++ C/C-03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dimarque <dimarque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 16:23:43 by dimarque          #+#    #+#             */
-/*   Updated: 2022/07/11 18:00:35 by dimarque         ###   ########.fr       */
+/*   Updated: 2022/07/13 15:44:16 by dimarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ int	ft_strncmp(char *s1,char *s2,unsigned int n)
 	unsigned char b;
 
 	i = 0;
-/* 	a = s1[i];
-	b = s2[i]; */
 	if (n != 0)
 	{
 		a = s1[i];
@@ -34,83 +32,4 @@ int	ft_strncmp(char *s1,char *s2,unsigned int n)
 	return (a - b);
 }
 
-/* #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void	ft_strncmp_test(char *s1, char *s2, unsigned int n)
-{
-	int res;
-	int ft_res;
-
-	res = strncmp(s1, s2, n);
-	ft_res = ft_strncmp(s1, s2, n);
-	if (res != ft_res)
-		printf("> KO, expected: %d, got: %d\n", res, ft_res);
-	else
-		printf("> OK, result: %d\n", ft_res);
-}
-
-int		main(void)
-{
-	char	*s1 = "abcdefghi";
-	char	*s2 = "abcdEfghi";
-	char	s3[] = {'a', 'b', -23, 'd', 'e', 'f', 'g', 'h', 'i', '\0'};
-	char	*s4 = "abcdefghi";
-	char	*s5 = "abcd";
-	char	*s6 = "abcdefghijklmn";
-	char	*s7 = "";
-
-	int		offset;
-
-	offset = 0;
-	printf("[1] Testing strings \"%s\" and \"%s\"\n", s1, s2);
-	while (offset < 10)
-	{
-		ft_strncmp_test(s1, s2, offset);
-		offset++;
-	}
-	offset = 0;
-	printf("[2] Testing strings \"%s\" and \"%s\"\n", s1, s3);
-	while (offset < 10)
-	{
-		ft_strncmp_test(s1, s3, offset);
-		offset++;
-	}
-	offset = 0;
-	printf("[3] Testing strings \"%s\" and \"%s\"\n", s2, s3);
-	while (offset < 10)
-	{
-		ft_strncmp_test(s2, s3, offset);
-		offset++;
-	}
-	offset = 0;		// Comparing equal strings. Should return 0.
-	printf("[4] Testing strings \"%s\" and \"%s\" (same strings)\n", s1, s4);
-	while (offset < 10)
-	{
-		ft_strncmp_test(s1, s4, offset);
-		offset++;
-	}
-	offset = 0;		// Comparing with a shorter string (s5 lenght 5).
-	printf("[5] Testing strings \"%s\" and \"%s\"\n", s1, s5);
-	while (offset < 10)
-	{
-		ft_strncmp_test(s1, s5, offset);
-		offset++;
-	}
-	offset = 0;		// Comparing with a longer string (s6 lenght 15).
-	printf("[6] Testing strings \"%s\" and \"%s\"\n", s1, s6);
-	while (offset < 10)
-	{
-		ft_strncmp_test(s1, s6, offset);
-		offset++;
-	}
-	offset = 0;		// Comparing with an empty string (s7).
-	printf("[7] Testing strings \"%s\" and \"%s\"\n", s1, s7);
-	while (offset < 10)
-	{
-		ft_strncmp_test(s1, s7, offset);
-		offset++;
-	}
-	return (0);
-} */
+// https://github.com/eduardomosko/fake-moulinnete/blob/main/c03-tests/ex01.c 
--- C/C-03/ex03/ft_strncat.c
+++ C/C-03/ex03/ft_strncat.c
@@ -6,14 +6,14 @@
 /*   By: dimarque <dimarque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 19:14:01 by dimarque          #+#    #+#             */
-/*   Updated: 2022/07/11 19:23:05 by dimarque         ###   ########.fr       */
+/*   Updated: 2022/07/13 15:36:22 by dimarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	unsigned int i;
-	unsigned int j;
+	unsigned int	i;
+	unsigned int	j;
 
 	i = 0;
 	j = 0;
@@ -30,73 +30,4 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	return (dest);
 }
 
-/* #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char	*ft_strncat(char *dest, char *src, unsigned int nb);
-
-int	main(void)
-{
-	int 	nb;
-	char	*src;
-	char	*dest;
-	char	*ft_src;
-	char	*ft_dest;
-	char	*buff;
-	char	*ft_buff;
-
-	src = calloc(7, sizeof(char));
-	dest = calloc(12, sizeof(char));
-	ft_src = calloc(12, sizeof(char));
-	ft_dest = calloc(12, sizeof(char));
-	strcpy(src, "galera");
-	strcpy(ft_src, "galera");
-	strcpy(dest, "alo ");
-	strcpy(ft_dest, "alo ");
-	nb = 13;
-	buff = strncat(dest, src, nb);
-	ft_buff = ft_strncat(ft_dest, ft_src, nb);
-	if (ft_dest != ft_buff)
-		printf("[1] KO, return value from ft_strncat is different of ft_dest. ft_strncat=%p ft_dest=%p\n", ft_buff, ft_dest);
-	else if (strcmp(buff, ft_buff) != 0)
-	{
-		printf("[1] KO, strcmp between ft_dest and dest is different of 0(%d)\n", strcmp(buff, ft_buff));
-		return (1);
-	}
-	else
-		printf("[1] OK, nb = %02d, result=%s\n", nb, ft_buff);
-	nb = 4;
-	strcpy(src, "galera");
-	strcpy(ft_src, "galera");
-	strcpy(dest, "alo ");
-	strcpy(ft_dest, "alo ");
-	buff = strncat(dest, src, nb);
-	ft_buff = ft_strncat(ft_dest, ft_src, nb);
-	if (ft_dest != ft_buff)
-		printf("[2] KO, return value from ft_strncat is different of ft_dest. ft_strncat=%p ft_dest=%p\n", ft_buff, ft_dest);
-	else if (strcmp(buff, ft_buff) != 0)
-	{
-		printf("[2] KO, strcmp between ft_dest and dest is different of 0(%d)\n", strcmp(buff, ft_buff));
-		return (1);
-	}
-	else
-		printf("[2] OK, nb = %02d, result=%s\n", nb, ft_buff);
-	nb = 0;
-	strcpy(src, "galera");
-	strcpy(ft_src, "galera");
-	strcpy(dest, "alo ");
-	strcpy(ft_dest, "alo ");
-	buff = strncat(dest, src, nb);
-	ft_buff = ft_strncat(ft_dest, ft_src, nb);
-	if (ft_dest != ft_buff)
-		printf("[3] KO, return value from ft_strncat is different of ft_dest. ft_strncat=%p ft_dest=%p\n", ft_buff, ft_dest);
-	else if (strcmp(buff, ft_buff) != 0)
-	{
-		printf("[3] KO, strcmp between ft_dest and dest is different of 0(%d)\n", strcmp(buff, ft_buff));
-		return (1);
-	}
-	else
-		printf("[3] OK, nb = %02d, result=%s\n", nb, ft_buff);
-	return (0);
-} */
+// https://github.com/eduardomosko/fake-moulinnete/blob/main/c03-tests/ex03.c
