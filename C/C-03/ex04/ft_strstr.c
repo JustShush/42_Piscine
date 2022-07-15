020ea1e5b83220798ab9462c720ca461d3d05e62
dimarque <dimarque@c1r17s15.42lisboa.com>
2022-07-15 11:32:36 +0100

    C-03 	modified:   C/C-03/ex04/ft_strstr.c

--- C/C-03/ex04/ft_strstr.c
+++ C/C-03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: dimarque <dimarque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:48:00 by dimarque          #+#    #+#             */
-/*   Updated: 2022/07/14 13:15:26 by dimarque         ###   ########.fr       */
+/*   Updated: 2022/07/15 11:31:18 by dimarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,49 +36,11 @@ char	*ft_strstr(char *str, char *to_find)
 		}
 		if (to_find[i] == '\0')
 			return (&str[j]);
-		else 
+		else
 			restart(&count, &i, &j);
 		count++;
 	}
 	return (0);
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char	*ft_strstr(char *str, char *to_find);
-
-int	main(void)
-{
-	char *str;
-	char *to_find;
-	char *buff;
-	char *ft_buff;
-
-	str = strcpy(calloc(11, sizeof(char)), "alo galera");
-	to_find = strcpy(calloc(7, sizeof(char)), "galera");
-	buff = strstr(str, to_find);
-	ft_buff = ft_strstr(str, to_find);
-	if (buff != ft_buff)
-		printf("[1] KO, substrings don't match(expected:%s, got:%s)\n", buff, ft_buff); 
-	else
-		printf("[1] OK. expected: %s result: %s\n", buff, ft_buff);
-	str = strcpy(calloc(11, sizeof(char)), "alo galera");
-	to_find = strcpy(calloc(7, sizeof(char)), "galerA");
-	buff = strstr(str, to_find);
-	ft_buff = ft_strstr(str, to_find);
-	if (buff != ft_buff)
-		printf("[2] KO, substrings don't match(expected:%s, got:%s)\n", buff, ft_buff); 
-	else
-		printf("[2] OK. expected: %s result: %s\n", buff, ft_buff);
-	str = strcpy(calloc(11, sizeof(char)), "alo galera");
-	to_find = strcpy(calloc(1, sizeof(char)), "");
-	buff = strstr(str, to_find);
-	ft_buff = ft_strstr(str, to_find);
-	if (buff != ft_buff)
-		printf("[2] KO, substrings don't match(expected:%s, got:%s)\n", buff, ft_buff); 
-	else
-		printf("[2] OK. expected: %s result: %s\n", buff, ft_buff);
-	return (0);
-}
+// https://github.com/eduardomosko/fake-moulinnete/blob/main/c03-tests/ex04.c
